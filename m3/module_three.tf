bf028be40e2d9693ef6f45b771698d3e3defbbe3
y-usuf <m.yusuf7@yandex.com>
2021-06-15 23:31:23 +0100

    First push

--- m3/module_three.tf
+++ m3/module_three.tf
@@ -1,30 +1,4 @@
-##################################################################################
-# VARIABLES
-##################################################################################
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "private_key_path" {}
-variable "key_name" {}
-variable "region" {
-  default = "us-east-1"
-}
-
-
-##################################################################################
-# PROVIDERS
-##################################################################################
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
-}
-
-##################################################################################
-# DATA
-##################################################################################
-
+// Data
 data "aws_ami" "aws-linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -46,11 +20,8 @@ data "aws_ami" "aws-linux" {
 }
 
 
-##################################################################################
-# RESOURCES
-##################################################################################
-
-#This uses the default VPC.  It WILL NOT delete it on destroy.
+// Resources
+// This uses the default VPC.  It WILL NOT delete it on destroy.
 resource "aws_default_vpc" "default" {
 
 }
@@ -102,10 +73,7 @@ resource "aws_instance" "nginx" {
   }
 }
 
-##################################################################################
-# OUTPUT
-##################################################################################
-
+// output
 output "aws_instance_public_dns" {
   value = aws_instance.nginx.public_dns
 }
